c63820ddec98ab61bbebeb7c1489f6bc6307c8cc
mcc-17 <mcc-17@mcc-17s-Mac-mini.local>
2020-01-14 11:29:35 +0700

    Continue layout, add vue directive, and separate into client and server

---
+++ client/assets/js/main.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data:{
+        message: 'Mantap jiwa!',
+        pesan: 'You loaded this page on ' + new Date().toLocaleString(),
+        currentPage: 'publishedPage'
+    }
+})
--- main.js
+++
@@ -1,6 +0,0 @@
-$( document ).ready(function() {
-    $('#l1').click(event =>{
-        
-    })
-});
-
